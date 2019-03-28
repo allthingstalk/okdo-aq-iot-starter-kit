868d732aede94cbb7edc831a7845f1ae7cd85efb
AlainCuypers <39278921+AlainCuypers@users.noreply.github.com>
2019-03-28 14:30:00 +0100

    Example

---
+++ examples/OKdo_Air_Quality_IoT_Starter_Kit_V1/keys.h
@@ -0,0 +1,14 @@
+/****
+ * Enter your AllThingsTalk device credentials below
+ */
+#ifndef KEYS_h
+#define KEYS_h
+
+const char* DEVICE_ID = "Your Device ID";
+const char* DEVICE_TOKEN = "Your Device Token";
+
+
+const char* WIFI_SSID= "MyWifi";
+const char* WIFI_PASSWORD = "MyWifiPassword";
+
+#endif
